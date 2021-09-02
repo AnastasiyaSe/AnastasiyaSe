3732492720cef3058238119bcf7aac1575f033eb
AnastasiyaSe <79368124+AnastasiyaSe@users.noreply.github.com>
2021-09-02 13:03:41 +0300

    Delete application directory

--- application/dirty_main.py
+++
@@ -1,5 +0,0 @@
-from application_ import *
-
-if __name__ == '__main__':
-    calculate_salary()
-    get_employees()
--- application/main.py
+++
@@ -1,5 +0,0 @@
-from application_ import people
-from application_ import salary
-if __name__ == '__main__':
-    calculate_salary()
-    get_employees()
